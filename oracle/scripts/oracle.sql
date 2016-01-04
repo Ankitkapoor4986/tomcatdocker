2d148da66ce805c82a94b6a5c3234f9026ce12b5
Ankit Kapoor <akapoor@xebia.com>
2016-01-04 11:45:50 +0530

    First commit

---
+++ oracle/scripts/oracle.sql
@@ -0,0 +1,26 @@
+declare
+userexist integer;
+begin
+  select count(*) into userexist from dba_users where username='WORKINGBRAIN_';
+  if (userexist = 1) then
+    execute immediate 'DROP USER WORKINGBRAIN_ CASCADE';
+  end if;
+end;
+/
+
+
+CREATE USER WORKINGBRAIN_ IDENTIFIED BY WORKINGBRAIN_;
+ALTER USER WORKINGBRAIN_ quota 50m ON system;
+
+GRANT CREATE SESSION TO WORKINGBRAIN_;
+GRANT CREATE TABLE TO WORKINGBRAIN_;
+
+ALTER SESSION SET CURRENT_SCHEMA =  WORKINGBRAIN_;
+
+CREATE TABLE website (
+title varchar(50) ,
+description varchar(50) ,
+url varchar(50) );
+
+INSERT INTO website (title, description, url) VALUES('Fuck you', 'I will fuck you', 'http://fucking.com/');
+COMMIT;
